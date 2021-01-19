23095581641fd5ebb800c08e5719ee5d84a264fc
Vitalii Zhuk <zhuk2205@gmail.com>
2021-01-19 11:41:02 +0200

    Add command for list possible consumers

---
+++ src/Command/ListConsumersCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the FiveLab Amqp package
+ *
+ * (c) FiveLab
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+declare(strict_types = 1);
+
+namespace FiveLab\Component\Amqp\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * The command for list possible consumers.
+ */
+class ListConsumersCommand extends Command
+{
+    private const DEFAULT_NAME = 'event-broker:consumer:list';
+
+    /**
+     * @var array|string[]
+     */
+    private $consumers;
+
+    /**
+     * Constructor.
+     *
+     * @param array  $consumers
+     * @param string $name
+     */
+    public function __construct(array $consumers, string $name = self::DEFAULT_NAME)
+    {
+        parent::__construct($name);
+
+        $this->consumers = $consumers;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('List of possible consumers.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Possible consumers are:');
+
+        foreach ($this->consumers as $consumer) {
+            $output->writeln(' * '.$consumer);
+        }
+
+        return 0;
+    }
+}
