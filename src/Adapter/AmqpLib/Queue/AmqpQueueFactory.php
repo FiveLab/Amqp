b38b22568f6121bc027ed1f74db19d77f75c3ce1
Vitalii Zhuk <zhuk2205@gmail.com>
2021-05-08 18:23:47 +0300

    fix for code style

--- src/Adapter/AmqpLib/Queue/AmqpQueueFactory.php
+++ src/Adapter/AmqpLib/Queue/AmqpQueueFactory.php
@@ -1,6 +1,15 @@
 <?php
 
-declare(strict_types=1);
+/*
+ * This file is part of the FiveLab Amqp package
+ *
+ * (c) FiveLab
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+declare(strict_types = 1);
 
 namespace FiveLab\Component\Amqp\Adapter\AmqpLib\Queue;
 
